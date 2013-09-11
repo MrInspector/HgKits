94e16ecdd26e5f44f5453e32988ae5b1f1008b08
MrInspector <wolfclash@hotmail.com>
2013-09-11 13:34:01 -0700

    Update HgKits.java

--- src/com/mrinspector/plugin/HgKits.java
+++ src/com/mrinspector/plugin/HgKits.java
@@ -1,5 +1,54 @@
 package com.mrinspector.plugin;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.World;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Sign;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionType;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.util.Vector;
+
+// THIS IMPORTS MAY NOT BE NEEDED!!!
+
 public class HgKits extends JavaPlugin {
   
   public void onEnable() {
@@ -8,4 +57,9 @@ public class HgKits extends JavaPlugin {
   
   public void onDisable() {
     // Disabling plugin
+  }
+
+  @EventHandler
+  public void onJoin(PlayerJoinEvent e){
+    e.getPlayer().sendMessage(ChatColor.GOLD + "[HgKits] Running Hg-Kits Version 1.0.0 by MrInspector!");
   }
